bbbfc8bee77735a4e6ae1d0474e8c3142c45b0e5
kookbyah <kookbyah@gmail.com>
2025-01-27 14:43:37 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculadora',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 149, 125, 173),
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Calculadora'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _output = "";
+  String _tempOutput = "";
+  String _operand = "";
+  double _num1 = 0;
+  double _num2 = 0;
+
+  void buttonPressed(String buttonText) {
+    if (buttonText == "Limpar") {
+      _tempOutput = "";
+      _output = "";
+      _num1 = 0;
+      _num2 = 0;
+      _operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "x" ||
+        buttonText == "/") {
+      if (_output.isNotEmpty) {
+        _num1 = double.parse(_output);
+      }
+      _operand = buttonText;
+      _tempOutput = "$_output $buttonText ";
+      _output = "";
+    } else if (buttonText == "=") {
+      if (_output.isNotEmpty) {
+        _num2 = double.parse(_output);
+      }
+      String calculation = "$_tempOutput$_output = ";
+
+      if (_operand == "+") {
+        _output = (_num1 + _num2).toString();
+      } else if (_operand == "-") {
+        _output = (_num1 - _num2).toString();
+      } else if (_operand == "x") {
+        _output = (_num1 * _num2).toString();
+      } else if (_operand == "/") {
+        _output = (_num1 / _num2).toString();
+      }
+
+      _tempOutput = calculation;
+      _operand = "";
+    } else {
+      _output += buttonText;
+    }
+
+    setState(() {});
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: ElevatedButton(
+          onPressed: () => buttonPressed(buttonText),
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(20.0),
+            backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+          ),
+          child: Text(
+            buttonText,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        title: Text(widget.title, style: const TextStyle(color: Colors.black)),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          width: 320,
+          height: 500,
+          margin: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10.0,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 24,
+                  horizontal: 12,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      _tempOutput,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      _output,
+                      style: const TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      buildButton("7"),
+                      buildButton("8"),
+                      buildButton("9"),
+                      buildButton("/"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      buildButton("4"),
+                      buildButton("5"),
+                      buildButton("6"),
+                      buildButton("x"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      buildButton("1"),
+                      buildButton("2"),
+                      buildButton("3"),
+                      buildButton("-"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      buildButton("0"),
+                      buildButton("."),
+                      buildButton("="),
+                      buildButton("+"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: ElevatedButton(
+                            onPressed: () => buttonPressed("Limpar"),
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 15.0,
+                              ),
+                              backgroundColor: Colors.redAccent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                            ),
+                            child: const Text(
+                              "Limpar",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
